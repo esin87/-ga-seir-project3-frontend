d9717e352dfc9ad5eaef973ae5ef72690cdb89d7
vbc221 <vbc221@nyu.edu>
2019-10-21 12:29:29 -0500

    beginning layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Dashboard from './components/dashboard';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav>
+        <h2>Dang-Good Desserts Zone</h2>
+        <ul>
+          <li><a className='navIcon' href='/'>Home</a></li>
+          <li><a className='navIcon' href='#'>Create Recipe</a></li>
+        </ul>
+      </nav>
+      <body>
+        <Dashboard/>
+      </body>
     </div>
   );
 }
---
+++ src/components/dashboard.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+class Dashboard extends Component{
+  constructor(){
+    super();
+  }
+  render(){
+    return(
+      <h3>hello world</h3>
+    )
+  }
+}
+
+export default Dashboard;
