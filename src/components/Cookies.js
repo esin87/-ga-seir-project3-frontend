7b19fc6bf4a498b9443f20b02573510e6b152aee
vbc221 <vbc221@nyu.edu>
2019-10-22 11:17:28 -0500

    changes

--- src/components/Cookies.js
+++ src/components/Cookies.js
@@ -4,7 +4,7 @@ import './Cookies.css';
 class Cookies extends Component{
   render() {
     return(
-      <div>
+      <div className='cookieBox'>
         <img src={require('../Project-3-Pictures/cookies/macaroon.png')} alt='cookies'></img>
         <h3>Cookies</h3>
       </div>
