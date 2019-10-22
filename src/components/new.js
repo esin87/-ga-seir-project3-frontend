0d64ea28e5571909f4c2cef984aea7d6b5b6cda7
Esin Saribudak <esin@Esins-Air.attlocal.net>
2019-10-21 20:18:55 -0500

    added new component and react router links

--- src/components/new.js
+++ src/components/new.js
@@ -1,42 +1,47 @@
 import React, { Component } from 'react';
+import './New.css';
 
 class New extends Component {
-    handleSubmit(evt){alert ("you are submitting your recipe");
-    evt.preventDefault()
-}
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        {/* //title */}
-        Title
-        <input type="text" name="title" />
-        {/* //category */}
-        Category
-        <select>
-          <option value="cakes">Cakes</option>
-          <option value="brownies">Brownies</option>
-          <option value="cookies">Cookies</option>
-          <option value="pastries">Pastries</option>
-          <option value="pies">Pies</option>
-          <option value="candies">Candies</option>
-          <option value="other">Other Desserts</option>
-        </select>
-        {/* //description */}
-        Description
-        <input type="text" name="description" />
-        {/* //items */}
-        Items
-        <input type="text" name="items" />
-        {/* //steps */}
-        Steps
-        <input type="text" name="steps" />
-        {/* //image */}
-        Image URL
-        <input type="text" name="image" />
-        <input type="submit" value="Submit" />
-      </form>
-    );
-  }
+	handleSubmit(evt) {
+		alert('you are submitting your recipe');
+		evt.preventDefault();
+	}
+	render() {
+		return (
+			<div>
+				<form onSubmit={this.handleSubmit}>
+					<h2>Create New Recipe</h2>
+					{/* //title */}
+					Title
+					<input type="text" name="title" />
+					{/* //category */}
+					Category
+					<select>
+						<option value="cakes">Cakes</option>
+						<option value="brownies">Brownies</option>
+						<option value="cookies">Cookies</option>
+						<option value="pastries">Pastries</option>
+						<option value="pies">Pies</option>
+						<option value="candies">Candies</option>
+						<option value="other">Other Desserts</option>
+					</select>
+					{/* //description */}
+					Description
+					<input type="text" name="description" />
+					{/* //items */}
+					Items
+					<input type="text" name="items" />
+					{/* //steps */}
+					Steps
+					<input type="text" name="steps" />
+					{/* //image */}
+					Image URL
+					<input type="text" name="image" />
+					<input type="submit" value="Submit" />
+				</form>
+			</div>
+		);
+	}
 }
 
 export default New;
