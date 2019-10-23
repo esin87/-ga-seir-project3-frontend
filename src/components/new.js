e0f887cd00cb24faec7f0a58f46a6a2d07a31ee0
vbc221 <vbc221@nyu.edu>
2019-10-23 10:41:53 -0500

    recipe css

--- src/components/new.js
+++ src/components/new.js
@@ -48,64 +48,67 @@ class New extends Component {
 	render() {
 		return (
 			<div className='newContainer'>
-				<form onSubmit={this.handleSubmit}>
-					<h2>Create New Recipe</h2>
-					{/* //title */}
-					Title
-					<input
-						name="title"
-						type="text"
-						value={this.state.title}
-						onChange={this.handleChange}
-					/>
-					{/* //category */}
-					Category
-					<select
-						name="category"
-						onChange={this.handleChange}
-						value={this.state.category}>
-						<option value="Cakes">Cakes</option>
-						<option value="Brownies">Brownies</option>
-						<option value="Cookies">Cookies</option>
-						<option value="Pastries">Pastries</option>
-						<option value="Pies">Pies</option>
-						<option value="Candies">Candies</option>
-						<option value="Other">Other Desserts</option>
-					</select>
-					{/* //description */}
-					Description
-					<input
-						name="description"
-						type="text"
-						value={this.state.description}
-						onChange={this.handleChange}
-					/>
-					{/* //items */}
-					Items
-					<input
-						name="items"
-						type="text"
-						value={this.state.items}
-						onChange={this.handleChange}
-					/>
-					{/* //steps */}
-					Steps
-					<input
-						name="steps"
-						type="text"
-						value={this.state.steps}
-						onChange={this.handleChange}
-					/>
-					{/* //image */}
-					Image URL
-					<input
-						name="image"
-						type="text"
-						value={this.state.image}
-						onChange={this.handleChange}
-					/>
-					<input type="submit" value="Submit" />
-				</form>
+				<div className='insideContainer'>
+						<form onSubmit={this.handleSubmit}>
+						<h2>Create New Recipe</h2>
+						{/* //title */}
+						Title
+						<input
+							name="title"
+							type="text"
+							value={this.state.title}
+							onChange={this.handleChange}
+						/>
+						{/* //category */}
+						Category
+						<select
+							name="category"
+							onChange={this.handleChange}
+							value={this.state.category}>
+							<option value="Cakes">Cakes</option>
+							<option value="Brownies">Brownies</option>
+							<option value="Cookies">Cookies</option>
+							<option value="Pastries">Pastries</option>
+							<option value="Pies">Pies</option>
+							<option value="Candies">Candies</option>
+							<option value="Other">Other Desserts</option>
+						</select>
+						{/* //description */}
+						Description
+						<input
+							name="description"
+							type="text"
+							value={this.state.description}
+							onChange={this.handleChange}
+						/>
+						{/* //items */}
+						Items
+						<input
+							name="items"
+							type="text"
+							value={this.state.items}
+							onChange={this.handleChange}
+						/>
+						{/* //steps */}
+						Steps
+						<input
+							name="steps"
+							type="text"
+							value={this.state.steps}
+							onChange={this.handleChange}
+						/>
+						{/* //image */}
+						Image URL
+						<input
+							name="image"
+							type="text"
+							value={this.state.image}
+							onChange={this.handleChange}
+						/>
+						<input type="submit" value="Submit" />
+					</form>
+				</div>
+				
 			</div>
 		);
 	}
