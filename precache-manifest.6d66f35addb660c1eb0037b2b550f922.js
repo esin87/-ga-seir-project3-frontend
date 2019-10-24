752478cd50d1bebddbdd7ca0758816f0a78e5be0
Esin Saribudak <esin@Esins-Air.attlocal.net>
2019-10-24 14:50:31 -0500

    latest updates

---
+++ precache-manifest.6d66f35addb660c1eb0037b2b550f922.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d49ffc469351f9be0e3e1c6231a8a01f",
+    "url": "/ga-seir-project3-frontend/index.html"
+  },
+  {
+    "revision": "9a00ce5a6e8449053a06",
+    "url": "/ga-seir-project3-frontend/static/css/main.3274570d.chunk.css"
+  },
+  {
+    "revision": "37787d495f325634d25c",
+    "url": "/ga-seir-project3-frontend/static/js/2.21bd3c78.chunk.js"
+  },
+  {
+    "revision": "9a00ce5a6e8449053a06",
+    "url": "/ga-seir-project3-frontend/static/js/main.c601da93.chunk.js"
+  },
+  {
+    "revision": "48fe890eb2b93d5f78bf",
+    "url": "/ga-seir-project3-frontend/static/js/runtime-main.6a895922.js"
+  },
+  {
+    "revision": "66b48c238a4fdd088e49794b131ec7cc",
+    "url": "/ga-seir-project3-frontend/static/media/applepie.66b48c23.png"
+  },
+  {
+    "revision": "ea9a3a21418a3bbc0fa6da3b7d8412f0",
+    "url": "/ga-seir-project3-frontend/static/media/baking.ea9a3a21.jpg"
+  },
+  {
+    "revision": "6ade738fafcc4b9116b4e6cf100be253",
+    "url": "/ga-seir-project3-frontend/static/media/brownie.6ade738f.png"
+  },
+  {
+    "revision": "26b3f24e161db881e08302794a8db82a",
+    "url": "/ga-seir-project3-frontend/static/media/category3.26b3f24e.jpg"
+  },
+  {
+    "revision": "7a897d3113814079b30d9017aec0fcec",
+    "url": "/ga-seir-project3-frontend/static/media/chocolatechip.7a897d31.png"
+  },
+  {
+    "revision": "1b750ef22bd9dd28c9680e66bdfd8d30",
+    "url": "/ga-seir-project3-frontend/static/media/donuts.1b750ef2.png"
+  },
+  {
+    "revision": "ab425f010ec92bf735bf8942df8b16a3",
+    "url": "/ga-seir-project3-frontend/static/media/ken-cake1.ab425f01.png"
+  },
+  {
+    "revision": "db4c161000521698955e84f784456d02",
+    "url": "/ga-seir-project3-frontend/static/media/kitkat.db4c1610.png"
+  },
+  {
+    "revision": "e03ce322b3506c86011116bfe457e663",
+    "url": "/ga-seir-project3-frontend/static/media/other.e03ce322.png"
+  },
+  {
+    "revision": "3d333e565419a1103ba950d6e471120b",
+    "url": "/ga-seir-project3-frontend/static/media/showBackG.3d333e56.jpg"
+  },
+  {
+    "revision": "d63f557711e7edb6c9a99a2e0a23e05a",
+    "url": "/ga-seir-project3-frontend/static/media/table2.d63f5577.jpg"
+  },
+  {
+    "revision": "0f34ea8b339ee28c7e9c786e13451f65",
+    "url": "/ga-seir-project3-frontend/static/media/tiramisu.0f34ea8b.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ga-seir-project3-frontend/precache-manifest.31ee560d028f3c57686424b9845a0587.js"
+  "/ga-seir-project3-frontend/precache-manifest.6d66f35addb660c1eb0037b2b550f922.js"
 );
 
 self.addEventListener('message', (event) => {
